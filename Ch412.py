0c9767295e7999364f960092a5e8097c8c9e932f
Jared Menze <digibyteme@gmail.com>
2018-09-20 10:33:02 -0500

    Added inputs

--- Ch412.py
+++ Ch412.py
@@ -5,7 +5,8 @@ for service in auto_services:
     print("%s -- $%d" % (service, auto_services[service]))
 
 #TODO Gather service inputs
-
+selections["Service 1"] = input("\nSelect first service:\n")
+selections["Service 2"] = input("Select second service:\n")
 
 #TODO Check if inputs are no service
 
