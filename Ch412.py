310b4cc89721b39dc167663322709a52f5460ded
Jared Menze <digibyteme@gmail.com>
2018-09-20 10:40:11 -0500

    Merged both things together and fixed resulting errors

--- Ch412.py
+++ Ch412.py
@@ -1,22 +1,25 @@
-auto_services = {"Oil change": 35, "Tire rotation": 19, "Car wash": 7, "Car wax": 12}
+#Jared's Part
+services = {"Oil change": 35, "Tire rotation": 19, "Car wash": 7, "Car wax": 12}
 
 print("Davy's auto shop services")
-for service in auto_services:
-    print("%s -- $%d" % (service, auto_services[service]))
+for service in services:
+    print("%s -- $%d" % (service, services[service]))
 
-#TODO Gather service inputs
-selections["Service 1"] = input("\nSelect first service:\n")
-selections["Service 2"] = input("Select second service:\n")
+#Gathering Inputs
+service1 = input("\nSelect first service:\n")
+service2 = input("Select second service:\n")
 
-#TODO Check if inputs are no service
+#Zach's Part
+
+#Checking inputs and printing invoice
 if service1 == '-': 
-    print('Service 1:',str(services[service1]))
+    print('Service 1: No service')
     print('Service 2:', service2 + ', $' + str(services[service2]))
     print()
     print('Total: $'+ str(services[service2]))
 elif service2 == '-':
     print('Service 1:', service1 + ', $' + str(services[service1]))
-    print('Service 2:', str(services[service2]))
+    print('Service 2: No service')
     print()
     print('Total: $'+ str(services[service1]))
 else:
@@ -24,5 +27,3 @@ else:
     print('Service 2:', service2 + ', $' + str(services[service2]))
     print()
     print('Total: $'+ str(services[service1] + services[service2]))
-
-#TODO Print invoice
