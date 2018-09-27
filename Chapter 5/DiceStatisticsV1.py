c90ef56065d25f869254a8f1e2e10c8f6091299a
Jared Menze <digibyteme@gmail.com>
2018-09-27 11:49:35 -0500

    Finished dice statistics by self

--- Chapter 5/DiceStatisticsV1.py
+++ Chapter 5/DiceStatisticsV1.py
@@ -1,24 +1,24 @@
 import random
 
-num_sixes = 0
-num_sevens = 0
 num_rolls = int(input('Enter number of rolls:\n'))
+dice_stats = {2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0, 11:0, 12:0}
 
-if num_rolls >= 1:
+while num_rolls >= 1:
     for i in range(num_rolls):
         die1 = random.randint(1,6)
         die2 = random.randint(1,6)
         roll_total = die1 + die2
 
-        #Count number of sixes and sevens
-        if roll_total == 6:
-            num_sixes = num_sixes + 1
-        if roll_total == 7:
-            num_sevens = num_sevens + 1
+        dice_stats[roll_total] += 1
+        
         print('Roll %d is %d (%d + %d)' % (i, roll_total, die1, die2))
 
     print('\nDice roll statistics:')
-    print('6s:', num_sixes)
-    print('7s:', num_sevens)
+    for stat in dice_stats:
+        if stat < 10:
+            print("%ds:  %s" % ((stat), ('*' * dice_stats[stat])))
+        else:
+            print("%ds: %s" % (stat, ('*' * dice_stats[stat])))
+    num_rolls = int(input('\nEnter number of rolls:\n'))
 else:
-    print('Invalid number of rolls. Try again.')
+    print('Goodbye')
