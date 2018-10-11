2efb03ebbf7041f8a6fdfe00f96fc39e4e4a2e2d
Jared Menze <digibyteme@gmail.com>
2018-10-11 08:20:20 -0500

    Combined the codes

--- Chapter 6/Ch6WarmUp.py
+++ Chapter 6/Ch6WarmUp.py
@@ -1,7 +1,19 @@
-#TODO Add functions
-def shit():
-    return None
+def get_num_of_characters(inputStr):
+    char_list = (inputStr)
+    num_char = len(char_list)
+    return num_char
+
+def output_without_whitespace(inputStr):
+    whitespaces = [' ', '\t']
+    output = ''
+    for ch in inputStr:
+        if ch not in whitespaces:
+            output += ch
+    print("String with no whitespace:", output)
 
 if __name__ =='__main__':
-    #TODO Add all the shit here
-    shit()   
+    user_string = input('Enter a sentence or phrase:\n')
+    print()
+    print('You entered: %s\n' % (user_string))
+    print('Number of characters:', get_num_of_characters(user_string))
+    output_without_whitespace(user_string)
