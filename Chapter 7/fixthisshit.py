ebf007d133b4ea9a34ce539098d3994284bb3600
Zachary  Hagen <za_hagen@cis.lsc.local>
2018-10-25 10:08:06 -0500

    this is stupid

---
+++ Chapter 7/fixthisshit.py
@@ -0,0 +1,47 @@
+def get_title():
+    title = input('Enter a title for the data:\n')
+    return title
+
+def get_col1():
+    col1= input('Enter the column 1 header:\n')
+    return col1
+    
+def get_col2():    
+    col2 = input('Enter the column 2 header:\n')
+    return col2
+
+def data_point():
+    data = input('Enter a data point (-1 to stop input):\n')
+    while data != '-1':
+        shit = False
+        points = data.split(',')
+        
+        if ',' not in data:
+            print('Error: No comma in string.\n')
+        else: 
+            if data.count(',') > 1:
+                print('Error: Too many commas in input.\n')
+            else:
+                for i in range(1000):
+                    if points[1].strip() != str(i):
+                        shit = True
+                        break
+                    else:
+                        print('Error: Comma not followed by an integer.\n')
+        if shit:
+            print('Data string: {}'.format(points[0]))
+            print('Data integer:{}'.format(points[1]))
+        data = input('Enter a data point (-1 to stop input):\n')
+
+if __name__ == '__main__':
+    title = get_title()
+    print('You entered: %s' % title)
+    print()
+    col1 = get_col1()
+    print('You entered: %s' % col1)
+    print()
+    col2 = get_col2()
+    print('You entered: %s' % col2)
+    print()
+    data_point()
+    
